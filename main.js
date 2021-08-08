aab2f41a9426b532c02a6f8b626ba350faf54d64
rolfeshepshy <84261347+rolfeshepshy@users.noreply.github.com>
2021-08-07 20:41:49 -0700

    080721.001

--- main.js
+++ main.js
@@ -1,35 +1,8 @@
-// Make "stupidlogin" when clicked give message on screen
-// Message onscreen = "Nice Cock"
+const http = require('http');
+const express = require('express');
+const app = express()
 
-const loginButton = document.getElementById("stupidlogin");
+console.log(app)
 
-const message = document.createElement("p");
 
-const messageContainer = document.getElementById("message-container");
 
-message.innerHTML = "Nice Cock";
-message.style.margin = "0px";
-message.style.marginBottom = "0px";
-message.style.textAlign = "center";
-message.style.justifyContent = "center";
-message.style.marginTop = "0px";
-message.style.transition = "0.3s";
-messageContainer.style.borderRadius = "6px"
-let numberOfClicks = 0;
-
-function onClick (event) {
-
-    messageContainer.appendChild(message);
-    messageContainer.style.marginBottom = "40px";
-    numberOfClicks += 1;
-    if(numberOfClicks != 69){
-        message.innerHTML = "";
-        message.innerHTML = "Nice Cock " + "(" + numberOfClicks + ')';
-    } else{
-        message.innerHTML = "";
-        message.innerHTML = "Nice Cock " + "(" + numberOfClicks + ') ;) Nice.';
-    
-    }
-}
-
-loginButton.addEventListener("click", onClick);
